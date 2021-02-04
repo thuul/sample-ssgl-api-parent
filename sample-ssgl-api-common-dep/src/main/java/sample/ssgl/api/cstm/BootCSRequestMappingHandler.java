a92181993d74aa47a65eba3ef0ef9d8f059a2ed6
thuul <cm-ard@live.com>
2021-02-04 18:53:23 +0200

    remove target folder from Common Dep

--- sample-ssgl-api-common-dep/src/main/java/sample/ssgl/api/cstm/BootCSRequestMappingHandler.java
+++ sample-ssgl-api-common-dep/src/main/java/sample/ssgl/api/cstm/BootCSRequestMappingHandler.java
@@ -10,7 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.mvc.condition.RequestCondition;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
-import sample.ssgl.api.boot.cs.contrl.BootCustomErrorController;
 
 /**
  *
